cde766075c69ba8c02f396225db0d041c1020257
Siji Bamgboye <>
2020-02-21 22:16:48 +0100

    Created Basic UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+import Header from "./components/header/header";
+import Bckgrnd from "./assets/bckgrnd.jpg";
+class App extends React.Component {
+	render() {
+		return (
+			<div
+				className="App"
+				style={{
+					backgroundImage: `url(${Bckgrnd})`
+				}}
+			>
+				<Header />
+				<h1>My tasks</h1>
+				<input
+					placeholder="Ex: Write a new blog post"
+					maxLength={80}
+					type="text"
+				/>
+				<button>+</button>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./header.styles.css";
+
+function Header() {
+	return <div>To Do Or Not To Do</div>;
+}
+
+export default Header;
