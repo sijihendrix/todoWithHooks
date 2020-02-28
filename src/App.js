f1636894b906f6f75036707442933e00ab69bb3e
Siji Bamgboye <>
2020-02-28 18:31:23 +0100

    Added State Hooks to App.js. Added Material UI dependencies. Added Logic for TodoApp with Hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/header/header";
 import Bckgrnd from "./assets/bckgrnd.jpg";
-class App extends React.Component {
-	render() {
-		return (
-			<div
-				className="App"
-				style={{
-					backgroundImage: `url(${Bckgrnd})`
-				}}
-			>
-				<Header />
-				<h1>My tasks</h1>
-				<input
-					placeholder="Ex: Write a new blog post"
-					maxLength={80}
-					type="text"
-				/>
-				<button>+</button>
-			</div>
-		);
-	}
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemText from "@material-ui/core/ListItemText";
+
+function Todos({ todos }) {
+	// return (
+	// 	<div className="list">
+	// 		{todos.map((todo, i) => (
+	// 			<div className="task">{todo} jiiui </div>
+	// 		))}
+	// 	</div>
+	// );
+	return (
+		<List>
+			{todos.map((todo, index) => (
+				<ListItem key={index.toString()} dense button>
+					<Checkbox tabIndex={-1} disableRipple />
+					<ListItemText primary={todo} />
+				</ListItem>
+			))}
+		</List>
+	);
+}
+
+function App() {
+	const [todos, setTodos] = useState([]);
+	const [task, setTask] = useState("");
+
+	const handleInput = e => {
+		const input = e.target.value;
+		setTask(input);
+		console.log(task);
+	};
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		setTodos([...todos, task]);
+		setTask("");
+		console.log(todos);
+	};
+
+	return (
+		<div
+			className="App"
+			style={{
+				backgroundImage: `url(${Bckgrnd})`
+			}}
+		>
+			<Header />
+			<input
+				placeholder="Write a Task"
+				maxLength={80}
+				type="text"
+				className="input"
+				onChange={handleInput}
+			/>
+			<button onClick={handleSubmit}>+</button>
+
+			<Todos todos={todos} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./header.styles.css";
 
 function Header() {
-	return <div>To Do Or Not To Do</div>;
+	return <div className="header">To Do Or Not To Do</div>;
 }
 
 export default Header;
